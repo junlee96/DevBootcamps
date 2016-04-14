6222f20ee027bea05cdab63035d96827ba907db2
Wyman <wymanleejun@gmail.com>
2016-04-14 14:24:13 +0800

    Initial Commit

---
+++ DevBootCamps/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DevBootCamps
+//
+//  Created by Admin on 14/4/16.
+//  Copyright © 2016 Dreamsmart. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
